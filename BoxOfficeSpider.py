26b8f93c749304086e6384ba8ed9aa4244ca4bf3
Con Con <ckward@uci.edu>
2019-02-28 13:16:01 -0800

    box office

--- BoxOfficeSpider.py
+++ BoxOfficeSpider.py
@@ -1,9 +1,8 @@
 import scrapy
 from twisted.internet import reactor
 from scrapy.crawler import CrawlerRunner
-import json
-import requests
 from scrapy.utils.log import configure_logging
+from scrapy.selector import Selector
 
 class BoxOfficeSpider(scrapy.Spider):
     name = "boxoffice"
@@ -12,17 +11,10 @@ class BoxOfficeSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').get(),
-                'author': quote.css('small.author::text').get(),
-                'tags': quote.css('div.tags a.tag::text').getall(),
-            }
-        with open('test.txt', 'w') as f:
-            f.write("hello")
+        print(Selector(response=response).xpath('//title/text()').get())
 
 #running without command line
-#configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
+configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
 runner = CrawlerRunner()
 d = runner.crawl(BoxOfficeSpider)
 
