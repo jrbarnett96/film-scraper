f9c3f0ed4df0a5483355e8c3fecf23cf8b47aaf8
Jonathan Student <jrbarnett96@berkeley.edu>
2019-03-07 15:04:26 -0800

    Add OMDB and Mojo pipelines to pipelines.py

--- movie_scraper/pipelines.py
+++ movie_scraper/pipelines.py
@@ -5,20 +5,32 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-# to my understanding, we later implement the joining of both spiders output
-# as part of the pipeline
-
-import json
+from scrapy.exceptions import DropItem
 
 
 class BoxOfficeMojoPipeline(object):
-    """ Pipeline for processing Items scraped from Box Office Mojo box office records. """
+    """ Pipeline for processing Items scraped from Box Office Mojo"""
 
     def process_item(self, item, spider):
-        return
+        if item.get('categorical_data')['Response'] == 'False':
+            raise DropItem
+        else:
+            """ Cast numerical data into floats. """
+            item['worldwide'] = float(item['worldwide'][1:])
+            item['domestic'] = float(item['domestic'][1:])
+            item['domestic_share'] = float(item['domestic_share'][:-1])
+            item['overseas'] = float(item['overseas'][1:])
+            item['overseas_share'] = float[item['overseas_share'][1:]]
+            if len(item['year']) > 4:
+                item['year'] = item['year'][:-1]
+            return item
 
-    def open_spider(self, spider):
-        return
 
-    def close_spider(self, spider):
-        return
+class OMDBPipeline(object):
+    """ Pipeline for processing categorical data from OMDB, reinserting the processed data. """
+
+    def process_item(self, item, spider):
+        if item.get('categorical_data')['Response'] == 'False':
+            raise DropItem
+        else:
+            categorical = item.get('categorical_data')
